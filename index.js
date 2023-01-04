82d4bf9332d80f03ee68e27826cf4eb15d429e75
reshad21 <miakala7@gmail.com>
2023-01-04 12:34:28 +0600

    user table create successfully

--- index.js
+++ index.js
@@ -4,30 +4,48 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
- 
+
 // middleware
 app.use(cors());
 app.use(express.json());
- 
 
 
 
 
-app.get('/',(req,res)=>{
+
+app.get('/', (req, res) => {
     res.send('server is running');
 })
- 
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gplljg9.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
- 
-app.listen(port,()=>{
+async function run() {
+    try {
+
+        const usersCollection = client.db("webSpiderDb").collection("users");
+
+        // =============== user collection code =============== //
+        app.post('/user', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            // browser a dakha jabe na karon ata post method tai console a dakha jbe data server theke send hobe kina
+            const result = await usersCollection.insertOne(user);
+            res.send(result)
+
+        })
+
+
+
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
     console.log(`Listening to the port ${port}`);
 })
